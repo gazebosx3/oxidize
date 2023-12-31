e737f05bf4d59ea5628c6de1dba31c11245f29d6
Sam Glass <samuelglass@gmail.com>
2023-12-30 19:40:02 -0500

    textinput

--- src/components/drink-screen.tsx
+++ src/components/drink-screen.tsx
@@ -1,28 +1,52 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useState } from "react";
-import { View } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
 import { RootStackParamList } from "../types";
+import drinkDB from "../drink-data";
 
 
-type DrinkProps = NativeStackScreenProps<RootStackParamList, 'Drink', 'MyStack'>;
 
+type DrinkProps = NativeStackScreenProps<
+  RootStackParamList,
+  "Drink",
+  "MyStack"
+>;
 export default function DrinkScreen({ route, navigation }: DrinkProps) {
-    const { passedDrinkId, sessionDrinks, setSessionDrinks } = route.params;
-  
-    const [drinkId, setDrinkId] = useState(passedDrinkId);
 
-    if (drinkId) {
-        return (
-            <View>
-                Edit/Remove Drink here
-            </View>
-        )
-    } else {
-        return (
-            <View>
-                Add drink should go here
-                {/* Some kind of search for drinks. Can incorporate "Add your own drink" later */}
-            </View>
-        )
-    }
+  const [text, onChangeText] = useState("Useless Text");
+  const [number, onChangeNumber] = useState("");
+  
+  const { passedDrinkId, sessionDrinks, setSessionDrinks } = route.params;
+
+  const [drinkId, setDrinkId] = useState(passedDrinkId);
+
+  if (drinkId) {
+    return <View>Edit/Remove Drink here</View>;
+  } else {
+    return (
+      <View>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="useless placeholder"
+          keyboardType="numeric"
+        />
+      </View>
+    );
   }
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
